d889cf70bab5332536429ba7f181c1db30ab0ff0
georstav <gistavraka@yahoo.gr>
2018-02-24 03:25:55 +0200

    First Commit to Git for the Project Museum in a Suitcase

---
+++ Museum in a Suitcase/Assets/C# Scripts/Splash Screen/SplashScreenToMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashScreenToMenu : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SceneManager.LoadScene("Tutorial");
+        //Goes from Splash Screen to the first scene which is the Turorial
+
+    }
+
+}
+
